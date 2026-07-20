0968b2500e4dd51b3ba6dab3c399e5624b122fdf
Matheus-Sounier <matheus.sounier2024@gmail.com>
2026-07-20 14:58:48 -0400

    updates: detection option being able to detect a running video besides a image

--- src/recognition/detector.py
+++ src/recognition/detector.py
@@ -1,3 +1,4 @@
+import time
 import cv2
 import mediapipe as mp
 from mediapipe.tasks import python as mp_python
@@ -8,7 +9,8 @@ from config import MODEL_PATH
 base_options = mp_python.BaseOptions(model_asset_path=MODEL_PATH)
 
 detector_options = vision.FaceDetectorOptions(
-    base_options=base_options
+    base_options=base_options,
+    running_mode=vision.RunningMode.VIDEO,
 )
 
 detector = vision.FaceDetector.create_from_options(
@@ -20,5 +22,5 @@ def detect_faces(img):
         image_format=mp.ImageFormat.SRGB,
         data=cv2.cvtColor(img, cv2.COLOR_BGR2RGB),
     )
-
+    timestamp_ms = int(time.perf_counter() * 1000)  
     return detector.detect(mp_image)
