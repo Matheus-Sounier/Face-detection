36a0980592b1215ab5dd0e217d76755a55108753
Matheus-Sounier <matheus.sounier2024@gmail.com>
2026-07-20 14:57:33 -0400

    chore: adds frame count and 'every N' to lower the cpu consume - waits until face is stable and 'cooldown is ok' to print and send to database

--- src/main.py
+++ src/main.py
@@ -12,11 +12,21 @@ camera = CameraStream(index=0, use_dshow=True)
 
 last_sent_time = 0
 COOLDOWN_LOCAL = 2
+frame_count = 0
+PROCESS_EVERY_N_FRAMES = 3
+face_stable_since = None
+STABILITY_HOLD_TIME = 0.3
 
 while True:
     success, img = camera.read()
-
     if not success:
+        continue
+
+    frame_count += 1
+    if frame_count % PROCESS_EVERY_N_FRAMES != 0:
+        cv2.imshow("Facial Access Control", img)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
         continue
 
     result = detect_faces(img)
@@ -36,14 +46,20 @@ while True:
 
             now = time.time()
 
-            if now - last_sent_time > COOLDOWN_LOCAL:
+            if face_stable_since is None:
+                face_stable_since = now
+
+            is_stable = now - face_stable_since >= STABILITY_HOLD_TIME
+            cooldown_ok = now - last_sent_time > COOLDOWN_LOCAL
+
+            if is_stable and cooldown_ok:
                 last_sent_time = now
 
                 image_bytes = crop_face(img, x, y, w, h)
-
                 data = recognize_face(image_bytes)
-
                 draw_result(img, x, y, data)
+    else:
+        face_stable_since = None
 
     cv2.imshow("Facial Access Control", img)
 
